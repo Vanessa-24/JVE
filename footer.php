7cf0a15e5fb90bc904c9329414a28b931990686d
justneedhelp43 <justinalim43@gmail.com>
2021-10-28 00:30:32 +0800

    Frontend done for invoice; started on checkout

--- footer.php
+++ footer.php
@@ -2,21 +2,21 @@
       <div class="footer-wrapper">
         <ul>
           <li class="footer-heading">Shop</li>
-          <li><a href="smartphones.html">Smartphones</a></li>
-          <li><a href="">Laptops</a></li>
-          <li><a href="">Desktops</a></li>
-          <li><a href="">Watches</a></li>
-          <li><a href="">Earbuds</a></li>
+          <li><a href="product_catalogue.php?category=Smartphones">Smartphones</a></li>
+          <li><a href="product_catalogue.php?category=Laptops">Laptops</a></li>
+          <li><a href="product_catalogue.php?category=Desktops">Desktops</a></li>
+          <li><a href="product_catalogue.php?category=Watches">Watches</a></li>
+          <li><a href="product_catalogue.php?category=Earbuds">Earbuds</a></li>
         </ul>
         <ul>
           <li class="footer-heading">Support</li>
-          <li><a href="contact-us.html">Contact Us</a></li>
+          <li><a href="contact-us.php">Contact Us</a></li>
           <li><a href="faq.html">FAQs</a></li>
         </ul>
         <ul>
           <li class="footer-heading">JVE</li>
           <li><a href="aboutus.html">About Us</a></li>
-          <li><a href="stores.html">Stores</a></li>
+          <li><a href="stores.php">Stores</a></li>
         </ul>
         <div class="break"></div>
         <div class="copyright">
