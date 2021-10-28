e0d0948bd1d91ed6a474057d826effbe5b045194
Vanessa <59255274+Vanessa-24@users.noreply.github.com>
2021-10-28 20:17:56 +0800

    formating

--- products.php
+++ products.php
@@ -268,17 +268,17 @@
     <header class="header">
         <nav>
             <a href="./">
-            <img class="jve-logo" alt="JVE Logo" src="img/JVE-logo.png" />
+                <img class="jve-logo" alt="JVE Logo" src="img/JVE-logo.png" />
             </a>
             <div class="header-products">
-            <a href="product_catalogue.php?category=Smartphones">Smartphones</a>
-            <a href="product_catalogue.php?category=Laptops">Laptops</a>
-            <a href="product_catalogue.php?category=Desktops">Desktops</a>
-            <a href="product_catalogue.php?category=Watches">Watches</a>
-            <a href="product_catalogue.php?category=Earbuds">Earbuds</a>
+                <a href="product_catalogue.php?category=Smartphones">Smartphones</a>
+                <a href="product_catalogue.php?category=Laptops">Laptops</a>
+                <a href="product_catalogue.php?category=Desktops">Desktops</a>
+                <a href="product_catalogue.php?category=Watches">Watches</a>
+                <a href="product_catalogue.php?category=Earbuds">Earbuds</a>
             </div>
             <a href="shopcart.php">
-            <img alt="shopcart" src="img/bag_icon.svg" />
+                <img alt="shopcart" src="img/bag_icon.svg" />
             </a>
         </nav>
     </header>
